48dcc1014b799e06ca827991c326fb426ed9b33d
clay <2868902382@qq.com>
2016-10-10 16:00:35 +0800

    修改完成

---
+++ FreeCodeCamp/js/css.js
@@ -0,0 +1,17 @@
+$(function(){
+    var style="<style>.map-fixed-header {"+
+        "width: auto;"+
+        "padding-left: 30px;"+
+    "}  .map-accordion {"+
+    "margin-left: 350px;"+
+    "margin-top: 0;"+
+    "}  h2,h3 {"+
+    "font-size: 14px;"+
+    "margin: 0!important;"+
+    "padding: 0!important;"+
+    "}  #nested-collapseJointoFreeCodeCamp{"+
+    "margin-bottom: 0;"+
+    "}</style>";
+    // console.log(style);
+    $("html").append(style);
+})
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # chrome-extension
+
+## FreeCodeCamp
+
+[https://freecodecamp.cn/map](https://freecodecamp.cn/map)此页面字体太大不方便整体浏览查看，
+写个extension改一下，同时改变布局
+
+### 改变之后：
+![改变之后](after.PNG)
+
